1f227db3a620f37b043961b355f2ec73e2101326
yararagi <montanaroyari@gmail.com>
2023-05-02 22:32:41 +0200

    init finestra e agiunta file json per la compilazione

---
+++ main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<raylib.h>
+#include<time.h>
+
+unsigned int displayX;
+unsigned int displayY;
+
+void initWindow(void);
+
+int main(void){
+
+    initWindow();
+
+    return 0;
+}
+
+void initWindow(void){
+    InitWindow(800, 450, "prj");
+
+    int display = GetCurrentMonitor();
+    displayX=GetMonitorWidth(display);
+    displayY=GetMonitorHeight(display);
+
+    SetWindowSize(displayX, displayY);
+    SetWindowState(FLAG_FULLSCREEN_MODE);
+    
+    SetTargetFPS(60);
+    HideCursor();
+}
